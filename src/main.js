9a9398a62b8aac587495f0feab1dd6c6dff30efe
Kris Hopper <hopperdavis@gmail.com>
2021-06-29 09:44:35 -0700

    add promise

--- src/main.js
+++ src/main.js
@@ -2,51 +2,27 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+import WeatherService from './weather-service.js'
+
+function clearFields()  {
+  $('#location').val("");
+  $('.showErrors').text("");
+  $('.showHumidity').text("");
+  $('.showTemp').text("");
+}
 
 $(document).ready(function () {
   $('#weatherLocation').click(function () {
-    const city = $('#location').val();
-    $('#location').val("");
-    const zip = $('#zip').val();
-    $('#zip').val("");
-
-    if (!city) {
-      let zipRequest = new XMLHttpRequest();
-      const zipUrl = `http://api.openweathermap.org/data/2.5/weather?zip=${zip}&appid=${process.env.API_KEY}`;
-  
-      zipRequest.onreadystatechange = function () {
-        if (this.readyState === 4 && this.status === 200) {
-          const response = JSON.parse(this.responseText);
-          getElements(response);
-        }
-      };
-  
-      zipRequest.open("GET", zipUrl, true);
-      zipRequest.send();
-
-    } else {
-      let request = new XMLHttpRequest();
-      const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.API_KEY}`;
-  
-      request.onreadystatechange = function () {
-        if (this.readyState === 4 && this.status === 200) {
-          const response = JSON.parse(this.responseText);
-          getElements(response);
-        }
-      };
-  
-      request.open("GET", url, true);
-      request.send();
-    }
-    const kelToFahr = (kelvins) => {
-      return Math.round((kelvins - 273.15) * 9/5 + 32)
-    }
-
-    function getElements(response) {
-      $('.showHumidity').text(`The humidity in ${city} is ${response.main.humidity}%`);
-      $('.showTemp').text(`The temperature in Fahrenheit is ${kelToFahr(response.main.temp)} degrees.`);
-      $('.showFeelsLike').text(`The temperature feels more like ${kelToFahr(response.main.feels_like)} degrees.`);
-    }
+    let city = $('#location').val();
+    clearFields();
+    let promise = WeatherService.getWeather(city);   
+    promise.then(function(response) {
+      const body = JSON.parse(response);
+      $('.showHumidity').text(`The humidity in ${city} is ${body.main.humidity}%`);
+      $('.showTemp').text(`The temperature in Kelvins is ${body.main.temp} degrees.`);
+    }, function(error)  {
+      $('.showErrors').text(`There was an error processing your request: ${error}`);
+    });
   });
 });
 
